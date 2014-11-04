4c1ad88cd4f90061753a89a56b8566d83a6fe06b
David Eriksson <david@2good.nu>
2014-11-04 21:16:32 +0100

    App to show SMSC for messages in SMS inbox

---
+++ app/src/main/java/se/divideandconquer/smsc/Messages.java
@@ -0,0 +1,90 @@
+package se.divideandconquer.smsc;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorWrapper;
+import android.net.Uri;
+import android.provider.Telephony;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+
+public class Messages extends ActionBarActivity {
+
+    private static final String TAG = "MyActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messages);
+
+        final ListView listview = (ListView) findViewById(R.id.listView);
+
+        String[] columns = new String[]{
+                Telephony.TextBasedSmsColumns.BODY,
+                Telephony.TextBasedSmsColumns.SERVICE_CENTER
+        };
+
+        // the XML defined views which the data will be bound to
+        int[] to = new int[]{
+                R.id.textView,
+                R.id.textView2,
+        };
+
+        Cursor cursor = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+        listview.setAdapter(new SimpleCursorAdapter(this, R.layout.message_item, new CursorWrapper(cursor) {
+
+            @Override
+            public String getString(int columnIndex) {
+
+                String value = super.getString(columnIndex);
+                if (columnIsServiceCenter(columnIndex)) {
+                    if (value.equals("+46705008999")) {
+                        value += " Telia";
+                    } else if (value.equals("+46708000708")) {
+                        value += " Telenor";
+                    } else if (value.equals("+46707990001") || value.equals("+46707990002") || value.equals("+46707990003")) {
+                        value += " Comviq";
+                    } else if (value.equals("+46735480000") || value.equals("46735480011")) {
+                        value += " Tre";
+                    }
+                }
+                return value;
+            }
+
+            private boolean columnIsServiceCenter(int columnIndex) {
+                return columnIndex == getColumnIndex(Telephony.TextBasedSmsColumns.SERVICE_CENTER);
+            }
+
+
+        }, columns, to, 0));
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.messages, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
